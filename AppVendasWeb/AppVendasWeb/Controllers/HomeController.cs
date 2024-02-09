8c1076d85304fe7c09d102e2ecfdd0dffc108465
Felipe Salmazo <felipesalmazo123@gmail.com>
2024-02-09 14:53:54 -0300

    Resolvendo

--- AppVendasWeb/AppVendasWeb/Controllers/HomeController.cs
+++ AppVendasWeb/AppVendasWeb/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace AppVendasWeb.Controllers
         {
             return View();
         }
+        public IActionResult TestIndex()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
